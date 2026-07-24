f2e4fd4d7e8b2a397e9d280b529a779da99fc93e
Kanayan <glglntn6@gmail.com>
2026-07-24 14:47:32 +0900

    chore: abc083-b,abc-088-b

--- problems/abc083-b/Main.ts
+++ problems/abc083-b/Main.ts
@@ -5,4 +5,16 @@ const inputSource = process.env.ATCODER_INPUT_FILE ?? 0;
 const input = readFileSync(inputSource, "utf8").trim().split(/\s+/);
 
 // ここに解答を実装する
-void input;
+const [n, a, b] = input;
+const numN = Number(n);
+const numA = Number(a);
+const numB = Number(b);
+
+let sum = 0;
+for (let index = 0; index <= numN; index++) {
+    let indexSum = 0;
+    String(index).split("").forEach(x => indexSum += Number(x));
+    if (indexSum >= numA && indexSum <= numB)
+        sum += index;
+}
+console.log(sum);
--- problems/abc088-b/Main.ts
+++ problems/abc088-b/Main.ts
@@ -5,4 +5,14 @@ const inputSource = process.env.ATCODER_INPUT_FILE ?? 0;
 const input = readFileSync(inputSource, "utf8").trim().split(/\s+/);
 
 // ここに解答を実装する
-void input;
+input.shift();
+const numInput = input.map(Number)
+let alice = 0;
+let bob = 0;
+for (let index = 0; numInput.length != 0; index++) {
+    const maxNum = Math.max(...numInput);
+    numInput.splice(numInput.lastIndexOf(maxNum), 1)
+    index % 2 === 0 ? alice += maxNum : bob += maxNum
+}
+
+console.log(alice - bob);
