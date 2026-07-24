72ea7a7b9d9fc37dec9347b005b07e2f8c087d39
Kanayan <glglntn6@gmail.com>
2026-07-24 20:34:13 +0900

    chore: abc085c - Otoshidama

--- problems/abc085-c/Main.ts
+++ problems/abc085-c/Main.ts
@@ -5,4 +5,28 @@ const inputSource = process.env.ATCODER_INPUT_FILE ?? 0;
 const input = readFileSync(inputSource, "utf8").trim().split(/\s+/);
 
 // ここに解答を実装する
-void input;
+const [N, Y] = input.map(Number)
+let isLogged = false
+const checkSum = (x: number, y: number, z: number) => {
+    if (isLogged) return false;
+    const sum = 10000 * x + 5000 * y + 1000 * z;
+    if (Y === sum) {
+        console.log(`${x} ${y} ${z}`)
+        isLogged = true;
+        return true;
+    }
+    return false;
+}
+
+for (let iX = 0; iX < (Y / 10000) + 1; iX++) {
+    if (isLogged) break
+    for (let iY = 0; iY < (Y / 5000) + 1; iY++) {
+        if (isLogged) break
+        const iZ = N - (iX + iY)
+        if (iZ < 0) continue
+        if ((iX + iY + iZ) != N) continue
+        checkSum(iX, iY, iZ)
+    }
+}
+
+if (!isLogged) console.log("-1 -1 -1")
