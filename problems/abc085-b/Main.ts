9a8a4dc1a88b01f4ea453168e9d5dee509d8d6bb
Kanayan <glglntn6@gmail.com>
2026-07-24 18:57:05 +0900

    chore: 085-b

--- problems/abc085-b/Main.ts
+++ problems/abc085-b/Main.ts
@@ -5,4 +5,15 @@ const inputSource = process.env.ATCODER_INPUT_FILE ?? 0;
 const input = readFileSync(inputSource, "utf8").trim().split(/\s+/);
 
 // ここに解答を実装する
-void input;
+input.shift()
+const numInput = input.map(Number)
+let result = 0
+let beforeNum = 0
+for (let i = 0; numInput.length != 0; i++) {
+    const maxNum = Math.max(...numInput)
+    numInput.splice(numInput.lastIndexOf(maxNum), 1)
+    if (beforeNum !== maxNum) result++
+    beforeNum = maxNum
+}
+
+console.log(result);
