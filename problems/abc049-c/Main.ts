8f1c64ad239476436d463bf848d7cf907bbe531e
Kanayan <glglntn6@gmail.com>
2026-07-25 13:02:17 +0900

    chore: ABC049C

--- problems/abc049-c/Main.ts
+++ problems/abc049-c/Main.ts
@@ -5,4 +5,16 @@ const inputSource = process.env.ATCODER_INPUT_FILE ?? 0;
 const input = readFileSync(inputSource, "utf8").trim().split(/\s+/);
 
 // ここに解答を実装する
-void input;
+const [s] = input
+const words = ["dream", "dreamer", "erase", "eraser"]
+
+const a = Array<boolean>(s.length + 1).fill(false)
+a[0] = true
+for (let i = 0; i < s.length; i++) {
+    if (!a[i]) continue
+    for (const w of words) {
+        if (s.startsWith(w, i))
+            a[i + w.length] = true
+    }
+}
+console.log(a[s.length] ? "YES" : "NO")
